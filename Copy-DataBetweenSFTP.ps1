bb77287d0ed2ac6517d7ed5d11c7b854843fa022
Justin Cooper <jcooper@chicousd.org>
2025-10-30 09:59:19 -0700

    initial

--- Copy-DataBetweenSFTP.ps1
+++ Copy-DataBetweenSFTP.ps1
@@ -1,89 +1,114 @@
 # Framework: Powershell CUSD template for vendor student data upload
-# This is just a sample template to serve as an example of how SQL and SFTP tasks can be automated.
 # Please familiarize yourself with the code and packages and use at your own risk
-# Extract file(s) and send via SFTP
+# Copy file(s) between two SFTP servers
+# Requires the Posh-SSH module: Install-Module -Name Posh-SSH
 [cmdletbinding()]
 param (
- [Parameter(Mandatory = $True)][string]$SourceSFTPServer,
- [Parameter(Mandatory = $True)][int]$SourceSFTPPort = 22,
- [Parameter(Mandatory = $True)][System.Management.Automation.PSCredential]$SourceSFTPCredential,
- [Parameter(Mandatory = $true)][string]$SourceSFTPDirectory,
- [Parameter(Mandatory = $true)][string]$SourceSFTPFileName,
- [Parameter(Mandatory = $True)][string]$DestinationSftpServer,
- [Parameter(Mandatory = $True)][int]$DestinationSftpPort = 22,
- [Parameter(Mandatory = $True)][System.Management.Automation.PSCredential]$DestinationSftpCredential,
- [Parameter(Mandatory = $True)][string]$DestinationSFTPDirectory,
- [Parameter(Mandatory = $True)][string]$DestinationSFTPFileName,
+ [Parameter(Mandatory = $True)][Alias('srcSrv')][string]$SourceSFTPServer,
+ [Parameter(Mandatory = $false)][Alias('srcPort')][int]$SourceSFTPPort = 22,
+ [Parameter(Mandatory = $True)][Alias('srcCred')][System.Management.Automation.PSCredential]$SourceSFTPCredential,
+ [Parameter(Mandatory = $true)][Alias('srcDir')][string]$SourceSFTPDirectory,
+ [Parameter(Mandatory = $true)][Alias('srcFile')][string]$SourceSFTPFileName,
+ [Parameter(Mandatory = $True)][Alias('destSrv')][string]$DestinationSftpServer,
+ [Parameter(Mandatory = $false)][Alias('destPort')][int]$DestinationSftpPort = 22,
+ [Parameter(Mandatory = $True)][Alias('destCred')][System.Management.Automation.PSCredential]$DestinationSftpCredential,
+ [Parameter(Mandatory = $True)][Alias('destDir')][string]$DestinationSFTPDirectory,
+ [Parameter(Mandatory = $True)][Alias('destFile')][string]$DestinationSFTPFileName,
  [Alias('wi')][switch]$WhatIf
 )
 
-function Copy-ExportToRemote ($server, $port, $user, $exportedFilePath, $exportPath, $destinationDirectory) {
- Write-Host ('{0}' -f $MyInvocation.MyCommand.Name)
- $Session = New-SFTPSession -ComputerName $server -Credential $user -Port $port -AcceptKey:$true
- Set-SFTPItem -SessionId $Session.SessionId -Path $exportedFilePath -Destination $destinationDirectory -Force -Verbose
- Remove-SFTPSession -SessionId $Session.SessionId
-}
-
-function Get-RemoteData ($server, $user, $port, $rootDir, $sourceFile, $destinationDirectory) {
- Write-Host ('{0}' -f $MyInvocation.MyCommand.Name)
- $Session = New-SFTPSession -ComputerName $server -Credential $user -Port $port -AcceptKey:$true
- Get-SFTPItem -SessionId $Session.SessionId -Path (Join-Path -Path $rootDir -ChildPath $sourceFile) -Destination $destinationDirectory -Force -Verbose
- Remove-SFTPSession -SessionId $Session.SessionId
-}
-
-function New-DataDir ($dataPath) {
- if (Test-Path -Path $dataPath) { return }
- New-Item -Path $dataPath -ItemType Directory -Confirm:$false -Force
-}
-
-function New-Object () {
+function Copy-LatestToLocal {
  process {
-  Write-Host ('{0}' -f $MyInvocation.MyCommand.Name)
+  [string]$remoteFilePath = '{0}/{1}' -f $_.srcParams.remoteDirectory, $_.newName
+  Write-Host ('{0},{1},{2}' -f $MyInvocation.MyCommand.Name, $remoteFilePath, $_.localDataPath) -F Blue
+  Get-SFTPItem -SessionId $_.srcSession.SessionId -Path $remoteFilePath -Destination $_.localDataPath -Force -Verbose
+  return $_
+ }
+}
 
+function Copy-LatestToRemote {
+ process {
+  $_.localFilePath = (Join-Path -Path $_.localDataPath -ChildPath $_.newName)
+  Write-Host ('{0},{1},{2}' -f $MyInvocation.MyCommand.Name, $_.localFilePath, $_.destParams.remoteDirectory) -F Blue
+  Set-SFTPItem -SessionId $_.destSession.SessionId -Path $_.localFilePath -Destination $_.destParams.remoteDirectory -Force -Verbose
+  $_
+ }
+}
+
+function Get-LatestFile {
+ process {
+  [string]$matchName = $_.srcParams.fileName
+  Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $_.srcParams.fileName) -F Green
+  #TODO
+  $_.latestFile = Get-SFTPChildItem -SessionId $_.srcSession.SessionId -Path $_.srcParams.remoteDirectory |
+   Where-Object { ($_.LastWriteTime -gt (Get-Date).AddDays(-2)) -and ($_.FullName -match $matchName) } | Select-Object -First 1
+  Write-Host ('{0},Found: {1}' -f $MyInvocation.MyCommand.Name, $_.latestFile.FullName) -F Green
+  return $_
+ }
+}
+
+function New-CopyObject ($srcSession, $destSession, $srcParams, $destParams, $dataPath) {
+ process {
+  [PSCustomObject]@{
+   srcSession    = $srcSession
+   srcParams     = $srcParams
+   destSession   = $destSession
+   destParams    = $destParams
+   latestFile    = $null
+   localDataPath = $dataPath
+   localFilePath = $null
+   newName       = $null
+  }
+ }
+}
+
+function Rename-LatestFile {
+ process {
+  $_.newName = '{0}-{1}-{2}' -f $_.srcParams.remoteDirectory.Replace('/', ''), $_.srcParams.fileName.ToUpper(), (Get-Date -Format 'yyyy-MM-dd')
+  if ($_.latestFile.FullName -match $_.newName) {
+   Write-Host ('{0},No rename needed for {1}' -f $MyInvocation.MyCommand.Name, $_.latestFile.FullName) -F Yellow
+   return $_
+  }
+  Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $_.newName)
+  Rename-SFTPFile -SessionId $_.srcSession.SessionId -Path $_.latestFile.FullName -NewName $_.newName
+  $_
+ }
+}
+
+function Remove-LocalCopy {
+ process {
+  Write-Host ('{0},{1}' -f $MyInvocation.MyCommand.Name, $_.localFilePath) -F Yellow
+  Remove-Item -Path $_.localFilePath -Force -Confirm:$false
  }
 }
 
 # ==================== Main =====================
-
 # Imported Functions
 Import-Module -Name CommonScriptFunctions -Cmdlet New-SqlOperation, Show-BlockInfo
 Import-Module -Name Posh-SSH -Cmdlet New-SFTPSession, Set-SFTPItem, Remove-SFTPSession
 
-$outPath = '.\data\'
-# $sourceExportPath = (Join-Path -Path $outPath -ChildPath $SourceSFTPFileName)
-New-DataDir $outPath
-
-$fullExportPath = (Join-Path -Path $outPath -ChildPath $ExportName)
-
 $sourceParams = @{
- # Server           = $SourceSFTPServer
- # Port             = $SourceSFTPPort
- # Credential       = $SourceSFTPCredential
- RemoteDirectory  = $SourceSFTPDirectory
- FileName         = $SourceSFTPFileName
- DestinationPath  = $outPath
+ remoteDirectory = $SourceSFTPDirectory
+ sourcePath      = $SourceSFTPDirectory
+ fileName        = $SourceSFTPFileName
 }
-
 $destinationParams = @{
- # Server           = $DestinationSftpServer
- # Port             = $DestinationSftpPort
- # Credential       = $DestinationSftpCredential
- RemoteDirectory  = $DestinationSFTPDirectory
- FileName         = $DestinationSFTPFileName
- DestinationPath  = $fullExportPath
+ remoteDirectory = $DestinationSFTPDirectory
+ destinationPath = $DestinationSFTPDirectory
+ fileName        = $DestinationSFTPFileName
 }
-
+Write-Host ('{0},Connecting to Source SFTP {1}' -f $MyInvocation.MyCommand.Name, $SourceSFTPServer) -F Green
 $sourceSession = New-SFTPSession -ComputerName $SourceSFTPServer -Credential $SourceSFTPCredential -Port $SourceSFTPPort -AcceptKey
+Write-Host ('{0},Connecting to Destination SFTP {1}' -f $MyInvocation.MyCommand.Name, $DestinationSftpServer) -F Green
 $destinationSession = New-SFTPSession -ComputerName $DestinationSftpServer -Credential $DestinationSftpCredential -Port $DestinationSftpPort -AcceptKey
 
-Get-RemoteData $SourceSFTPServer $SourceSFTPPort $SourceSFTPCredential $SourceSFTPDirectory $SourceSFTPFileName $outPath
-# | Export-Csv -NoTypeInformation -Path $fullExportPath
-# $csvContent = Get-Content -Path $fullExportPath -Raw
-# Remove double quotes from the CSV content
-# $csvContent = $csvContent.Replace('"', '')
-# Write-Host "Double quotes removed from '$fullExportPath'."
-# Write the modified content back to the CSV file
-# Set-Content -Path $fullExportPath -Value $csvContent -Encoding UTF8
+$outPath = '.\data\'
 
-# Copy-ExportToRemote $SftpServer $DestinationSftpPort $SftpCredential $fullExportPath $RemoteDirectory
-# Remove-Item -Path $fullExportPath -Confirm:$false -Force
+New-CopyObject $sourceSession $destinationSession $sourceParams $destinationParams $outPath |
+ Get-LatestFile |
+  Rename-LatestFile |
+   Copy-LatestToLocal |
+    Copy-LatestToRemote |
+     Remove-LocalCopy
+
+Get-SFTPSession | Remove-SFTPSession
